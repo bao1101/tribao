721a42a888f97b50fd20674a4ff20588e83b6fae
bao1101 <baoho1112005@gmail.com>
2025-08-19 21:58:36 +0700

    Add files via upload

--- prj/src/controller.c
+++ prj/src/controller.c
@@ -3,24 +3,29 @@
 #include "system_data.h"
 #include "controller.h"
 
+#define REFRESH 1
+#define CHANGE_REFRESH_RATE 2
+#define BACKUP_CONFIG 3
+#define EXIT 4
+
 void handleUserInput(SystemData *data, int *refreshRate) {
     int choice;
     printf("1. Refresh\t2. Change Refresh Rate\t3. Backup Config\t4. Exit\n");
     scanf("%d", &choice);
     switch (choice) {
-        case 1:
+        case REFRESH:
             updateSystemData(data);
             checkAlerts(data);
             displayDashboard(data);
             break;
-        case 2:
+        case CHANGE_REFRESH_RATE:
             printf("Enter update frequency (seconds): ");
             scanf("%d", refreshRate);
             break;
-        case 3:
+        case BACKUP_CONFIG:
             backupConfig(data);
             break;
-        case 4:
+        case EXIT:
             exit(0);
         default:
             printf("Invalid choice!\n");
